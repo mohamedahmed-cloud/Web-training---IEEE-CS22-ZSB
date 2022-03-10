98c9015653d393c59ca069b71095165527f86623
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-03-10 03:28:16 +0200

    Finish Introduction Assignmnet

--- C.Task-3 - Javascript Part1/1.Introduction/4.Assignment_Four/main.js
+++ C.Task-3 - Javascript Part1/1.Introduction/4.Assignment_Four/main.js
@@ -7,9 +7,9 @@ console.log("Message Two");
 console.group("Grand Child Grouip");
 console.log("Message One");
 console.log("Message Two");
-console.groupEnd();
-console.groupEnd();
-console.groupEnd();
+console.groupEnd(); //To end Grand Group
+console.groupEnd(); // To End Child Group
+console.groupEnd(); //To End Group One
 console.group("Group 2");
 console.log("Message One");
 console.log("Message Two");
