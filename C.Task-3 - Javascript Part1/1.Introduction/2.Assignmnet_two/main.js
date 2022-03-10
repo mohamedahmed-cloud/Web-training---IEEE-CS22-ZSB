039cccb9191806be4a58cf41bc0ff973f15a34ca
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-03-10 03:26:37 +0200

    Finish Introduction Assignmnet

---
+++ C.Task-3 - Javascript Part1/1.Introduction/2.Assignmnet_two/main.js
@@ -0,0 +1,10 @@
+// using Directive
+// inside console
+// console.log("%cEzero","color:blue;font-size:24px;backgroun-colro:white");
+
+// inside Page
+document.write("<h1>Elzero</h1>");
+// document.querySelector("h1").style.color = "blue"
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.fontFamily = "Arial";
+document.querySelector("h1").style.textAlign = "center";
---
+++ C.Task-3 - Javascript Part1/1.Introduction/3.Assignment_Three/main.js
@@ -0,0 +1,8 @@
+// using Directive
+
+console.log(
+  "%c Elzero %c Web %c School",
+  "color:red;font-size:40px",
+  "color:green;font-size:40px;font-weight:bold",
+  "background-color:blue;font-size:40px"
+);
